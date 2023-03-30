96a2080ce0d3492bafffe1cb83c3eb7653360ae7
yash-explorer <yashgiradkar121@gmail.com>
2023-03-30 14:29:51 +0530

    Added css to the table

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const ejs = require('ejs');
+const { kStringMaxLength } = require('buffer');
+
+app.set('view engine', 'ejs');
+
+
+mongoose.connect('mongodb+srv://yash1:yash1@cluster0.txq9fum.mongodb.net/test');
+
+const AttSchema = {
+    Name: String,
+    Time: String,
+    Date: String
+}
+
+const pain = mongoose.model('pain', AttSchema, 'pain');
+
+app.get('/', (req, res) => {
+   
+    pain.find({})
+      .then(stds => {
+        res.render('index', {
+          stdList: stds
+        });
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send('Error fetching data');
+      });
+  });
+  
+  app.use(express.static('views'));
+app.listen(3000, function() {
+    console.log('server is running');
+})
